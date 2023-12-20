3169a9929780f0dc481b84c3bca41830fefdfaa7
Arpit Mathur <98715435+arpitmathur2412@users.noreply.github.com>
2023-12-20 11:48:51 +0000

    changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,18 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a developer and a data science enthusiast with a passion for solving problems and creating new things.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web Development (MERN)",
+  "Data Science",
+  "Cloud Computing (GCP)",
+  "Machine Learning",
+  "Deep Learning",
 ];
 
 /**
@@ -44,9 +43,7 @@ const skillsList = [
  * how you best work, or even a quote. This will help someone learn more
  * about you on a professional level.
  */
-const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
-
+const detailOrQuote = " ";
 const About = () => {
   return (
     <section className="padding" id="about">
