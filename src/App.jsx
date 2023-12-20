59a9a6c69369bd1aaf23b2b80e99fc0a19552a27
Arpit Mathur <98715435+arpitmathur2412@users.noreply.github.com>
2023-12-20 10:53:10 +0000

    updated info

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Arpit Mathur",
+  title: "Developer and Data Scientist",
+  email: "arpit.mathur@studentambassadors.com",
+  gitHub: "@arpitmathur2412",
+  instagram: "@_arpit.mathur",
+  linkedIn: "@arpitmathur24",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
